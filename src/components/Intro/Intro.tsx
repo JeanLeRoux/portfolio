6d0af95c457af775d650e2a97161f45bd7557596
JeanLeRoux <jeanlr14@gmail.com>
2021-03-13 14:06:50 +0200

    added intro block

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
-import '../Intro/Intro.scss'
+import '../Intro/Intro.scss';
+import personalImage from '../../Assets/personalImage.png';
 
 export default function Intro() {
     return (
         <div className={"component-background"}>
+            <div className={"navbar"}>
+                <h1 className={"logo"}>JLR<span className={"logo-dot"}>.</span></h1>
+                <div className={"link-container"}>
+                    <h3 className={"links"}>HOME</h3>
+                    <h3 className={"links"}>PORTFOLIO</h3>
+                    <h3 className={"links"}>CONTACT</h3>
+                </div>
+                
+            </div>
+            <div className={"content"}>
+                <div>
+                    <h1 className={"name-heading"}>Jean <br></br> Le Roux.</h1>
+                </div>
+                <div className={"image-container"}>
+                    <div className={"background-shape"}></div>  
+                    <img className={"personal-image"} src={personalImage}/>
+                </div>
+            </div>
             
         </div>
     )
