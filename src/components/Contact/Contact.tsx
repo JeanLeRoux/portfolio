fc845fcbd91ba7b4ded8182fc77b1ac8d125212a
JeanLeRoux <jeanlr14@gmail.com>
2021-03-13 12:45:52 +0200

    added image and contact

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,17 +1,35 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import Headings from '../Common/Headings/Headings'
 import '../Contact/Contact.scss'
 import ContactMethods from './ContactMethods'
 
+interface contact{
+    contactType:string,
+    details:string
+}
+
 const Contact: FC = () => {
+    const [contactDetails, setContactDetails] = useState<contact[]>([
+        {
+            "contactType":"Phone",
+            "details":"072 759 4041"
+        },
+        {
+            "contactType":"Linkedin",
+            "details":"http://linkedin.com/in/jean-le-roux-43306019b"
+        },
+        {
+            "contactType":"Email",
+            "details":"jeanlr14@gmail.com"
+        },
+    ]);
     return (
         <div className={"contact-background"}>
             <Headings headingOne={"CONTACT US"} headingTwo={"Ways To Contact Me"} paragraph={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}/>
         
             <div className={"contact-method-container"}> 
-                <ContactMethods/>
-                <ContactMethods/>
-                <ContactMethods/>
+                {contactDetails.map((information)=>(<ContactMethods contactType={information.contactType} details={information.details}/>))}
+                
             </div>
         </div>
     )
--- src/components/Contact/ContactMethods.tsx
+++ src/components/Contact/ContactMethods.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, {FC} from 'react';
+import 'font-awesome/css/font-awesome.min.css';
 
-import assets from "../Assets/email.svg"
-const ContactMethods =()=> {
+interface props{
+    contactType:string,
+    details:string,
+}
+
+
+const ContactMethods: FC<props> =({contactType,details})=> {
     return (
         <div className={"contact-div"}>
-            <h2 className={"contact-details"}>details</h2>
+            
+            <i className="fa fa-phone"></i>
+            <h2 className={"contact-details"}>{details}</h2>
         </div>
     )
 }
